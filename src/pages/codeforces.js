0f8dd9dae366285b7972e0e381c25c44b0f3b88b
cwlo2F <cwlo2F@gmail.com>
2024-12-03 05:00:16 +0900

    feat: Write codeforces page

--- src/pages/codeforces.js
+++ src/pages/codeforces.js
@@ -1,5 +1,86 @@
-// import * as React from "react"
+import * as React from "react"
+import { Link, graphql } from "gatsby"
 
-export default function Component() {
-    return "음, 여기에 Codeforces 관련 글을 모아보고 싶은데요. 어떻게 하는지 cwlo2F@gmail.com으로 알려주세요."
+import Bio from "../components/bio"
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const BlogIndex = ({ data, location }) => {
+    const siteTitle = data.site.siteMetadata?.title || `Title`
+    const posts = data.allMarkdownRemark.nodes
+
+    if (posts.length == 0) {
+        return (
+            <Layout location={location} title={siteTitle}>
+                <Bio />
+                <p>
+                    No blog posts found. Add markdown posts to "content/blog" (or the
+                    directory you specified for the "gatsby-source-filesystem" plugin in
+                    gatsby-config.js).
+                </p>
+            </Layout>
+        )
+    }
+
+    return (
+        <Layout location={location} title={siteTitle}>
+            <Bio />
+            <ol style={{ listStyle: `none` }}>
+                {posts.map(post => {
+                    const title = post.frontmatter.title || post.fields.slug
+
+                    return (
+                        <li key={post.fields.slug}>
+                            <article
+                                className="post-list-item"
+                                itemScope
+                                itemType="http://schema.org/Article"
+                            >
+                                <header>
+                                    <h2>
+                                        <Link to={post.fields.slug} itemProp="url">
+                                            <span itemProp="headline">{title}</span>
+                                        </Link>
+                                    </h2>
+                                    <small>{post.frontmatter.date}</small>
+                                </header>
+                            </article>
+                        </li>
+                    )
+                })}
+            </ol>
+        </Layout>
+    )
 }
+
+export default BlogIndex
+
+/**
+ * Head export to define metadata for the page
+ */
+
+export const Head = () => <Seo title="Codeforces" />
+
+export const pageQuery = graphql`
+  {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMarkdownRemark(sort: { frontmatter: { date: DESC } }, 
+      filter: { frontmatter: { tags: { in: ["Codeforces"] } } }) {
+      nodes {
+        excerpt
+        fields {
+          slug
+        }
+        frontmatter {
+          date(formatString: "MMMM DD, YYYY")
+          title
+          description
+        }
+      }
+    }
+  }
+`
